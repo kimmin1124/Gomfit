60f3e07573dacf3ec54b12a8fc7661d2678a0839
smug201911181 <70737546+smug201911181@users.noreply.github.com>
2021-05-21 02:38:21 +0900

    Add Resource

--- Game/game.py
+++ Game/game.py
@@ -1,48 +1,95 @@
 import pygame
 import random
 import datetime
-
-pygame.init()
-dWidth, dHeight = 500, 900
-
-## 윈도우 서피스 셋업 ##
-ourScreen = pygame.display.set_mode((dWidth, dHeight))
-pygame.display.set_caption("Game")
-
-## image Load ##
-def load_image(str):
-    return pygame.image.load("Images/" + str + ".png")
-im_title = load_image("title")
-
-clock = pygame.time.Clock()
+import time
+import os.path
 
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 128)
 BLACK = (0, 0, 0)
 
-GS_run = True;
-title = "Title"
-stage = "Stage"
-cur_scene = title;
-
-while GS_run:
-    nextStage = False
+## Class ##
+class GameManager():
+    def __init__(self, devMode = False):
+        self.devMode = devMode
+        self.tutorial = True
+        self.window_size = (500, 900)
+        self.display = pygame.display.set_mode(self.window_size)
+        self.title = "Game"
+        self.clock = pygame.time.Clock()
+        self.game_is_running = False
+        self.fontObj = pygame.font.Font("Fonts/나눔스퀘어B.ttf", 32)
+        self.textSurfaceObj = fontObj.render("Hello World!", True, BLACK)
+        self.textRectObj = textSurfaceObj.get_rect()
+        self.textRectObj.center = (630, 32)
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            nextStage = True
-            
-    if nextStage and cur_scene is title:
-        cur_scene = stage
+    def start_game(self):
+        pygame.init()
+        pygame.display.set_caption(self.title)
+        self.gameState = "Title"
         
-    if cur_scene is title:
-        ourScreen.fill((255,255,255))
-        ourScreen.blit(im_title, (-350 , -100))
-        pygame.display.flip()
-    if cur_scene is stage:
-        ourScreen.fill(BLUE)
-        pygame.display.flip()
-pygame.quit()
+    def draw_background(self, image):
+        self.display.fill((255,255,255))
+        self.display.blit(image, (-350, -100))
+        
+
+## Utility ##
+def load_image(str):
+    path = os.path.sep.join(["Images", str + ".png"])
+    return pygame.image.load(path)
+
+def ui(gm):
+    
+
+if __name__ == "__main__":
+    print("Game Start")
+    gm = GameManager()
+    gm.start_game()
+    im_title = load_image("title")
+    im_ingame = load_image("background")
+    print("Loof Coming")
+    while True:
+        time.sleep(2.0)
+        
+        if gm.gameState == "Title":
+            print("in the Title")
+            ## Display Setting ##
+            gm.draw_background(im_title)
+            pygame.display.flip()
+            
+            ## Key Processing ##
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        gm.gameState = "InGame"
+                        
+        if gm.gameState == 'InGame':
+            ## Display Setting ##
+            gm.draw_background(im_ingame)
+            
+            ## 플레이 시간 출력
+            ## 일시정지 버튼 출력
+            ## 하트 출력
+            ## 화살표 출력
+            ## 카메라 출력
+            
+            pygame.display.flip()
+            
+            if gm.game_is_running:
+                whole_time = datetime.datetime.now()
+                
+            else:
+                record = datetime.datetime.now()
+            
+            ## Key Processing ##
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        print("다음 스테이지로")
+            
+
